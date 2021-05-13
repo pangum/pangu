b370937930580be0c3152af4300919d030a05201
storezhang <storezhang@gmail.com>
2021-05-13 13:36:31 +0800

    增加in和out

---
+++ in.go
@@ -0,0 +1,10 @@
+package pangu
+
+import (
+	`go.uber.org/dig`
+)
+
+// In 描述获得值
+type In struct {
+	dig.In
+}
---
+++ out.go
@@ -0,0 +1,10 @@
+package pangu
+
+import (
+	`go.uber.org/dig`
+)
+
+// Out 描述返回值
+type Out struct {
+	dig.Out
+}
