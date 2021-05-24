b3feeef1a38cc6497ff0b3f4b7a0578ab4e4ca50
storezhang <storezhang@gmail.com>
2021-05-24 12:58:09 +0800

    重构事务处理

--- session.go
+++ session.go
@@ -9,68 +9,69 @@ import (
 	`xorm.io/xorm`
 )
 
-// Session 事务控制
-type Session struct {
+// Tx 事务控制
+type Tx struct {
 	engine *xorm.Engine
 	logger glog.Logger
 }
 
-// NewSession 事务控制
-func NewSession(engine *xorm.Engine, logger glog.Logger) Session {
-	return Session{
+// NewTx 事务控制
+func NewTx(engine *xorm.Engine, logger glog.Logger) *Tx {
+	return &Tx{
 		engine: engine,
 		logger: logger,
 	}
 }
 
-func (s *Session) Tx(fun func(tx *xorm.Session) error, fields ...gox.Field) (err error) {
-	tx := s.engine.NewSession()
-	defer s.close(tx, fields...)
+func (t *Tx) Do(fun func(tx *xorm.Session) error, fields ...gox.Field) (err error) {
+	tx := t.engine.NewSession()
+	if err = t.begin(tx, fields...); nil != err {
+		return
+	}
+	defer t.close(tx, fields...)
 
 	if err = fun(tx); nil != err {
-		s.rollback(tx, fields...)
+		t.rollback(tx, fields...)
 	} else {
-		s.commit(tx, fields...)
+		t.commit(tx, fields...)
 	}
 
 	return
 }
 
-func (s *Session) commit(tx *xorm.Session, fields ...gox.Field) {
+func (t *Tx) begin(tx *xorm.Session, fields ...gox.Field) (err error) {
+	if err = tx.Commit(); nil != err {
+		t.error(err, "开始数据库事务出错", fields...)
+	}
+
+	return
+}
+
+func (t *Tx) commit(tx *xorm.Session, fields ...gox.Field) {
 	if err := tx.Commit(); nil != err {
-		fun, _, line, _ := runtime.Caller(1)
-
-		logFields := make([]gox.Field, 0, len(fields)+4)
-		logFields = append(logFields, field.String("fun", runtime.FuncForPC(fun).Name()))
-		logFields = append(logFields, field.Int("line", line))
-		logFields = append(logFields, fields...)
-		logFields = append(logFields, field.Error(err))
-		s.logger.Error("提交数据库事务出错", logFields...)
+		t.error(err, "提交数据库事务出错", fields...)
 	}
 }
 
-func (s *Session) close(tx *xorm.Session, fields ...gox.Field) {
+func (t *Tx) close(tx *xorm.Session, fields ...gox.Field) {
 	if err := tx.Close(); nil != err {
-		fun, _, line, _ := runtime.Caller(1)
-
-		logFields := make([]gox.Field, 0, len(fields)+4)
-		logFields = append(logFields, field.String("fun", runtime.FuncForPC(fun).Name()))
-		logFields = append(logFields, field.Int("line", line))
-		logFields = append(logFields, fields...)
-		logFields = append(logFields, field.Error(err))
-		s.logger.Error("关闭数据库事务出错", logFields...)
+		t.error(err, "关闭数据库事务出错", fields...)
 	}
 }
 
-func (s *Session) rollback(tx *xorm.Session, fields ...gox.Field) {
+func (t *Tx) rollback(tx *xorm.Session, fields ...gox.Field) {
 	if err := tx.Rollback(); nil != err {
-		fun, _, line, _ := runtime.Caller(1)
-
-		logFields := make([]gox.Field, 0, len(fields)+4)
-		logFields = append(logFields, field.String("fun", runtime.FuncForPC(fun).Name()))
-		logFields = append(logFields, field.Int("line", line))
-		logFields = append(logFields, fields...)
-		logFields = append(logFields, field.Error(err))
-		s.logger.Error("回退数据库事务出错", logFields...)
+		t.error(err, "回退数据库事务出错", fields...)
 	}
+}
+
+func (t *Tx) error(err error, msg string, fields ...gox.Field) {
+	fun, _, line, _ := runtime.Caller(1)
+
+	logFields := make([]gox.Field, 0, len(fields)+4)
+	logFields = append(logFields, field.String("fun", runtime.FuncForPC(fun).Name()))
+	logFields = append(logFields, field.Int("line", line))
+	logFields = append(logFields, fields...)
+	logFields = append(logFields, field.Error(err))
+	t.logger.Error(msg, logFields...)
 }
