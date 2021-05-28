c617bfb1b92c6b4c3d28717819d8e7b139ec72fe
storezhang <storezhang@gmail.com>
2021-05-28 13:33:47 +0800

    增加徽标

--- doc/docs/.vuepress/config.js
+++ doc/docs/.vuepress/config.js
@@ -12,12 +12,14 @@ module.exports = ctx => ({
     head: [
         ['link', {rel: 'icon', href: `/logo.png`}],
         ['link', {rel: 'manifest', href: '/manifest.json'}],
+        ['link', {rel: "icon", type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png"}],
+        ['link', {rel: "icon", type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png"}],
         ['meta', {name: 'theme-color', content: '#3eaf7c'}],
         ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
         ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
-        ['link', {rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png`}],
+        ['link', {rel: 'apple-touch-icon', href: `/icons/pangu-152x152.png`}],
         ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
-        ['meta', {name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png'}],
+        ['meta', {name: 'msapplication-TileImage', content: '/icons/pangu-144x144.png'}],
         ['meta', {name: 'msapplication-TileColor', content: '#000000'}]
     ],
     themeConfig: {
