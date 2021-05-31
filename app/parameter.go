9053a526e623864397b119a5e13e805ac25a7a99
storezhang <storezhang@gmail.com>
2021-05-31 14:59:49 +0800

    方法更能体现依赖注入概念

--- app/parameter.go
+++ app/parameter.go
@@ -1,7 +1,7 @@
 package app
 
 type parameter interface {
-	// Aliases 别名，可以是短名称也可以是长名称，比如一个叫version的命令，别名可以是[v,V,Version]
+	// Aliases 别名，可以是短名称也可以是长名称，比如一个叫version的命令，别名可以是[v,version]
 	Aliases() []string
 
 	// Name 名称
