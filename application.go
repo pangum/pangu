383f269794401ea97d0ca724643e7133295b4e45
storezhang <storezhang@gmail.com>
2021-06-10 08:11:58 +0800

    修复依赖关系返回error时，error被吞掉的问题

--- application.go
+++ application.go
@@ -188,9 +188,11 @@ func (a *Application) Run(bootstrap func(*Application) Bootstrap) (err error) {
 	}
 
 	// 启动应用
-	err = a.Invoke(func(startup *cli.App) error {
+	if err = a.Invoke(func(startup *cli.App) error {
 		return startup.Run(os.Args)
-	})
+	}); nil != err {
+		return
+	}
 
 	// 退出程序，解决最外层panic报错的问题
 	// 原理：如果到这个地方还没有发生错误，程序正常退出，外层panic得不到执行
