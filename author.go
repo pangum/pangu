6a1303ea0dcc87cc3f4263049ec52b9ac833394b
storezhang <storezhang@gmail.com>
2021-07-13 09:23:10 +0800

    修复依赖

--- author.go
+++ author.go
@@ -3,7 +3,7 @@ package pangu
 // Author 作者
 type Author struct {
 	// 名字
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name" yaml:"name" xml:"name"`
 	// 邮箱
-	Email string `json:"email" yaml:"email"`
+	Email string `json:"email" yaml:"email" xml:"email"`
 }
