bd9763e92a6dfca3239ae5f204c374bb8e362b64
storezhang <storezhang@gmail.com>
2022-03-23 20:39:53 +0800

    增加命名服务，方便开发服务

---
+++ app/serve_named.go
@@ -0,0 +1,17 @@
+package app
+
+// NamedServe 命名的服务
+type NamedServe struct {
+	name string
+}
+
+// 创建命名服务
+func NewNamedServe(name string) *NamedServe {
+	return &NamedServe{
+		name: name,
+	}
+}
+
+func (ns *NamedServe) Name() string {
+	return ns.name
+}
