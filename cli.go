43aee1904aa75ae1192ce19492ef7bd250468a2c
storezhang <storezhang@gmail.com>
2022-07-17 21:44:19 +0800

    refactor(命令行): 使用单引号代替双引号

--- cli.go
+++ cli.go
@@ -1,7 +1,7 @@
 package pangu
 
 import (
-	`github.com/urfave/cli/v2`
+	"github.com/urfave/cli/v2"
 )
 
 func newStartup() (startup *cli.App) {
@@ -11,15 +11,15 @@ func newStartup() (startup *cli.App) {
 	// 定制版本显示，版本号不可改变
 	startup.Version = Version
 	cli.VersionFlag = &cli.BoolFlag{
-		Name:    "version",
-		Aliases: []string{"v"},
-		Usage:   "显示应用程序版本信息",
+		Name:    `version`,
+		Aliases: []string{`v`},
+		Usage:   `显示应用程序版本信息`,
 	}
 	// 定制帮助信息
 	cli.HelpFlag = &cli.BoolFlag{
-		Name:    "help",
-		Aliases: []string{"h"},
-		Usage:   "显示所有命令或者帮助信息",
+		Name:    `help`,
+		Aliases: []string{`h`},
+		Usage:   `显示所有命令或者帮助信息`,
 	}
 
 	return
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/pangum/pangu
 
-go 1.17
+go 1.18
 
 require (
 	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
