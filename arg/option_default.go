82646f40d80f630df033cd52a487da1de89c4983
storezhang <storezhang@gmail.com>
2022-11-13 20:51:49 +0800

    feat(参数): 增加常用参数列表

--- arg/option_default.go
+++ arg/option_default.go
@@ -6,11 +6,17 @@ import (
 
 var (
 	_        = String
+	_        = StringSlice
 	_        = Bool
+	_        = BoolSlice
 	_        = Int
+	_        = IntSlice
 	_        = Int64
+	_        = Int64Slice
 	_        = Float64
+	_        = Float64Slice
 	_        = Duration
+	_        = DurationSlice
 	_ option = (*optionDefault)(nil)
 )
 
@@ -18,9 +24,14 @@ type optionDefault struct {
 	_default any
 }
 
-// String 默认值
+// String 字符串默认值
 func String(value string) *optionDefault {
 	return _default(value)
+}
+
+// StringSlice 字符串列表默认值
+func StringSlice(values []string) *optionDefault {
+	return _default(values)
 }
 
 // Bool 默认值
@@ -28,9 +39,19 @@ func Bool(value bool) *optionDefault {
 	return _default(value)
 }
 
+// BoolSlice 默认值
+func BoolSlice(values []bool) *optionDefault {
+	return _default(values)
+}
+
 // Int 默认值
 func Int(value int) *optionDefault {
 	return _default(value)
+}
+
+// IntSlice 默认值
+func IntSlice(values []int) *optionDefault {
+	return _default(values)
 }
 
 // Int64 默认值
@@ -38,14 +59,29 @@ func Int64(value int64) *optionDefault {
 	return _default(value)
 }
 
+// Int64Slice 默认值
+func Int64Slice(values []int64) *optionDefault {
+	return _default(values)
+}
+
 // Float64 默认值
 func Float64(value float64) *optionDefault {
 	return _default(value)
 }
 
+// Float64Slice 默认值
+func Float64Slice(values []float64) *optionDefault {
+	return _default(values)
+}
+
 // Duration 默认值
 func Duration(value time.Duration) *optionDefault {
 	return _default(value)
+}
+
+// DurationSlice 默认值
+func DurationSlice(values []time.Duration) *optionDefault {
+	return _default(values)
 }
 
 func _default(_default any) *optionDefault {
