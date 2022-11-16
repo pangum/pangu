9e777487e4a3905ca937c49da5fc774b248000ef
storezhang <storezhang@gmail.com>
2022-11-16 13:57:52 +0800

    feat(上下文): 增加常用功能

--- app/context.go
+++ app/context.go
@@ -17,3 +17,11 @@ func NewContext(ctx *cli.Context) *Context {
 func (c *Context) String(name string) string {
 	return c.context.String(name)
 }
+
+func (c *Context) Set(name string, value string) error {
+	return c.context.Set(name, value)
+}
+
+func (c *Context) Value(name string) any {
+	return c.context.Value(name)
+}
