f20eab31f96f5b47f3aa55c1592ca26a74cf0df3
storezhang <storezhang@gmail.com>
2023-05-19 10:58:45 +0800

    feat(参数): 参数列表添加功能

--- app/arguments.go
+++ app/arguments.go
@@ -2,3 +2,22 @@ package app
 
 // Arguments 参数列表
 type Arguments []Argument
+
+func (a Arguments) Add(args ...Argument) (arguments Arguments) {
+	arguments = make([]Argument, len(a)+len(args))
+	index := 0
+	for _, arg := range args {
+		arguments[index] = arg
+		index++
+	}
+	for _, arg := range a {
+		arguments[index] = arg
+		index++
+	}
+
+	return
+}
+
+func (a Arguments) Append(args ...Argument) Arguments {
+	return append(a, args...)
+}
