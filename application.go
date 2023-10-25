312d1b489727b38bb221ee041a866f931862fbad
storezhang <storezhang@gmail.com>
2023-10-25 16:33:25 +0800

    docs(项目): 增加参数的描述

--- application.go
+++ application.go
@@ -8,8 +8,9 @@ import (
 var _ = New
 
 // Application 应用程序，可以加入两种种类型的程序
-// Serve 用于描述应用程序内的服务
-// Command 用于描述应用程序内可以被执行的命令
+// app.Serve 用于描述应用程序内的服务
+// app.Command 用于描述应用程序内可以被执行的命令
+// app.Argument 用于描述应用程序的参数
 type Application = core.Application
 
 func New() *builder.Application {
