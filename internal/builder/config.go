dfb17f2e2c822a0b00ae957c8a6d98acdb4c15d9
storezhang <storezhang@gmail.com>
2023-10-29 21:35:41 +0800

    feat(配置): 增加其它配置

--- internal/builder/config.go
+++ internal/builder/config.go
@@ -1,6 +1,7 @@
 package builder
 
 import (
+	"github.com/pangum/pangu/internal/internal/app"
 	"github.com/pangum/pangu/internal/internal/builder"
 	"github.com/pangum/pangu/internal/param"
 )
@@ -18,6 +19,55 @@ func NewConfig(application *Application) (config *Config) {
 	config.Config = builder.NewConfig(params)
 	config.params = params
 	config.application = application
+
+	return
+}
+
+func (c *Config) Path(path string) (config *Config) {
+	c.params.Paths = append(c.params.Paths, path)
+	config = c
+
+	return
+}
+
+func (c *Config) Extension(extension string) (config *Config) {
+	c.params.Extensions = append(c.params.Extensions, extension)
+	config = c
+
+	return
+}
+
+func (c *Config) Validate() (config *Config) {
+	c.params.Validate = true
+	config = c
+
+	return
+}
+
+func (c *Config) Invalidate() (config *Config) {
+	c.params.Validate = false
+	config = c
+
+	return
+}
+
+func (c *Config) Required() (config *Config) {
+	c.params.Nullable = false
+	config = c
+
+	return
+}
+
+func (c *Config) Nullable() (config *Config) {
+	c.params.Nullable = true
+	config = c
+
+	return
+}
+
+func (c *Config) Environment(key string, value string) (config *Config) {
+	c.params.Environments = append(c.params.Environments, app.NewEnvironment(key, value))
+	config = c
 
 	return
 }
