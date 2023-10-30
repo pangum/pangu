925e1245dc68ff7c8de243d168f3f0bb5d03e94c
storezhang <storezhang@gmail.com>
2023-10-30 09:02:12 +0800

    feat(应用): 增加退出标识

--- internal/command/serve.go
+++ internal/command/serve.go
@@ -18,9 +18,10 @@ var _ app.Command = (*Serve)(nil)
 type Serve struct {
 	*Default
 
-	serves []app.Serve
-	signal chan os.Signal
-	logger app.Logger
+	serves  []app.Serve
+	signal  chan os.Signal
+	exiting bool
+	logger  app.Logger
 }
 
 func NewServe(logger app.Logger) *Serve {
@@ -40,31 +41,28 @@ func (s *Serve) Add(serves ...app.Serve) {
 func (s *Serve) Run(ctx *runtime.Context) (err error) {
 	count := len(s.serves)
 	if 0 != count {
-		s.logger.Info("启动服务开始", field.New("count", count))
-		if err = s.start(ctx); nil != err {
-			return
-		}
-		s.logger.Info("启动服务成功", field.New("count", count))
+		s.logger.Debug("启动所有服务开始", field.New("count", count))
+		err = s.start(ctx, count)
 	}
 
 	return
 }
 
-func (s *Serve) start(ctx *runtime.Context) (err error) {
+func (s *Serve) start(ctx *runtime.Context, count int) (err error) {
 	wg := new(sync.WaitGroup)
-	wg.Add(len(s.serves))
+	wg.Add(count)
 
 	for _, serve := range s.serves {
 		cloned := serve
 		go s.startServe(cloned, wg, &err)
 	}
+	s.logger.Debug("启动所有服务成功", field.New("count", count))
 
 	// 注册退出信号处理
 	signal.Notify(s.signal, syscall.SIGTERM, syscall.SIGINT)
 	<-s.signal
-	if err = s.stop(ctx); nil != err {
-		panic(err)
-	}
+	s.exiting = true
+	err = s.stop(ctx)
 
 	// 等待所有服务启动
 	wg.Wait()
@@ -78,7 +76,7 @@ func (s *Serve) startServe(serve app.Serve, wg *sync.WaitGroup, err *error) {
 	s.logger.Info("启动服务成功", field.New[string]("name", serve.Name()))
 	// 记录时间，如果发生错误的时间小于500毫秒，就是执行错误，应该立即退出；如果大于，则只记录日志
 	now := time.Now()
-	if se := serve.Start(); nil != se {
+	if se := serve.Start(); nil != se && !s.exiting {
 		errTime := time.Now()
 		if errTime.Sub(now) > 500*time.Millisecond {
 			s.logger.Info("服务执行错误", field.New[string]("name", serve.Name()), field.Error(se))
@@ -105,9 +103,10 @@ func (s *Serve) stop(_ *runtime.Context) (err error) {
 func (s *Serve) stopServe(serve app.Serve, wg *sync.WaitGroup, err *error) {
 	defer wg.Done()
 
-	s.logger.Info("停止服务成功", field.New("name", serve.Name()))
 	if se := serve.Stop(); nil != se {
 		*err = se
 		s.logger.Info("停止服务出错", field.New("name", serve.Name()), field.Error(se))
+	} else {
+		s.logger.Info("停止服务成功", field.New("name", serve.Name()))
 	}
 }
