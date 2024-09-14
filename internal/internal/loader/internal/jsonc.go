9c55bd6dc3a77f68f7a7170087c3ead09fc4961d
storezhang <storezhang@gmail.com>
2024-09-14 11:21:31 +0800

    feat(配置): 完善json5支持

--- internal/internal/loader/internal/jsonc.go
+++ internal/internal/loader/internal/jsonc.go
@@ -15,7 +15,7 @@ type Jsonc struct {
 	len         int    // 长度
 	objectDepth uint   // 对象深度
 	arrayDepth  uint   // 数组深度
-	inStr       bool   // 是否在字符串中
+	inString    bool   // 是否在字符串中
 	inArray     bool   // 是否在数组中
 	inObject    bool   // 是否在对象中
 	last        string // 最后一个有效非空格字符
@@ -51,39 +51,42 @@ func (j *Jsonc) Strip(from string) (to string) {
 		original, prev, char, next = j.getSegments(from, prev)
 
 		// 处理十六进制数据
-		if j.isNonStringValue(char, "0") && (next == "x" || next == "X") {
+		if j.isNonStringValue(char, "0") && ("x" == next || "X" == next) {
 			to += j.hexadecimal(from)
 			continue
 		}
-
-		quote, quoted = j.quoteKey(char, quoted)
-		to += quote
-
-		// 修剪数组或对象末尾的尾部逗号
-		if j.comment == 0 && !j.inStr && ((j.inArray && char == "]") || (j.inObject && char == "}")) {
-			to = j.comma.ReplaceAllString(to, `$1`)
-		}
-
-		j.checkArrayObject(char)
-
 		// 果内部字符串或外部注释，则按原样附加
-		if j.inString(prev, char, next, original) || j.outsideComment(char, next) {
+		if j.insideString(prev, char, next, original) || j.outsideComment(char, next) {
 			to += j.compliment(prev, char, next)
 			continue
 		}
 
-		// 清除评论周围的尾随空格
-		if j.hasCommentEnded(char, next) && char == "\n" {
-			to = strings.TrimRight(to, "\r\n\t") + char
-		}
+		j.strip(&to, char, next, &quote, &quoted)
 	}
 
 	return
 }
 
+func (j *Jsonc) strip(to *string, char string, next string, quote *string, quoted *bool) {
+	*quote, *quoted = j.quoteKey(char, *quoted)
+	*to += *quote
+
+	// 修剪数组或对象末尾的尾部逗号
+	if j.comment == 0 && !j.inString && ((j.inArray && char == "]") || (j.inObject && char == "}")) {
+		*to = j.comma.ReplaceAllString(*to, `$1`)
+	}
+
+	j.checkArrayObject(char)
+
+	// 清除评论周围的尾随空格
+	if j.hasCommentEnded(char, next) && char == "\n" {
+		*to = strings.TrimRight(*to, "\r\n\t") + char
+	}
+}
+
 func (j *Jsonc) hexadecimal(from string) (to string) {
-	j.index++
 	hexadecimal := ""
+	j.index++
 	for j.index < j.len {
 		char := from[j.index : j.index+1]
 		if !j.isNumber(char, true) {
@@ -110,7 +113,7 @@ func (j *Jsonc) isNumber(char string, hex bool) (number bool) {
 }
 
 func (j *Jsonc) outsideComment(char, next string) bool {
-	if !j.inStr && j.comment == 0 {
+	if !j.inString && j.comment == 0 {
 		if char+next == "//" {
 			j.comment = 1
 		}
@@ -122,44 +125,46 @@ func (j *Jsonc) outsideComment(char, next string) bool {
 	return 0 == j.comment
 }
 
-func (j *Jsonc) compliment(prev string, char string, next string) (compliment string) {
-	if j.inStr && char == constant.Esc && constant.Enter == next {
+func (j *Jsonc) compliment(prev string, char string, next string) (final string) {
+	if j.inString && char == constant.Esc && constant.Enter == next {
 		j.index++
-		compliment = ""
-	} else if cached, ok := j.spacesPair[char]; ok && j.inStr {
-		compliment = cached
+		final = ""
+	} else if cached, ok := j.spacesPair[char]; ok && j.inString {
+		final = cached
 	} else if j.isNonStringValue(char, "+") && j.isNumber(next, false) {
-		compliment = ""
+		final = ""
 	} else if j.isNonStringValue(char, ".") {
-		compliment = j.decimalPointNumber(prev, next)
+		final = j.decimalPointNumber(prev, next)
 	} else if constant.SingleQuote == j.stringLabel {
-		compliment = j.singleQuotedString(prev, char, next)
+		final = j.singleQuotedString(prev, char, next)
+	} else {
+		final = char
 	}
 
 	return
 }
 
-func (j *Jsonc) decimalPointNumber(prev string, next string) (point string) {
-	prevNum, nextNum := j.isNumber(prev, false), j.isNumber(next, false)
-	if !prevNum && nextNum {
-		point = "0."
-	} else if prevNum && !nextNum {
-		point = ".0"
+func (j *Jsonc) decimalPointNumber(prev string, next string) (final string) {
+	prevNumber := j.isNumber(prev, false)
+	nextNumber := j.isNumber(next, false)
+	if !prevNumber && nextNumber {
+		final = "0."
+	} else if prevNumber && !nextNumber {
+		final = ".0"
 	}
 
 	return
 }
 
-func (j *Jsonc) singleQuotedString(prev string, char string, next string) (single string) {
-	if char+next == constant.Esc+constant.SingleQuote {
-		char = constant.SingleQuote
+func (j *Jsonc) singleQuotedString(prev string, char string, next string) (final string) {
+	if constant.Esc+constant.SingleQuote == char+next {
 		j.index++
-	} else if prev != constant.Esc && char == constant.SingleQuote {
-		char = constant.DoubleQuote
-	} else if char == constant.DoubleQuote {
-		char = `\"`
+		final = constant.SingleQuote
+	} else if constant.Esc != prev && constant.SingleQuote == char {
+		final = constant.DoubleQuote
+	} else if constant.DoubleQuote == char {
+		final = `\"`
 	}
-	single = char
 
 	return
 }
@@ -186,39 +191,39 @@ func (j *Jsonc) checkArrayObject(char string) {
 }
 
 func (j *Jsonc) isNonStringValue(char, chars string) bool {
-	return !j.inStr && j.comment == 0 && strings.ContainsAny(char, chars)
+	return !j.inString && j.comment == 0 && strings.ContainsAny(char, chars)
 }
 
 func (j *Jsonc) quoteKey(char string, wasQuoted bool) (quote string, quoted bool) {
 	quoted = wasQuoted
 	inKey := j.inObject && j.comment == 0 && (j.last == "{" || j.last == ",")
-	if !j.inStr && inKey && !strings.ContainsAny(char, "[]{}'\",/*:\r\n\t ") {
+	if !j.inString && inKey && !strings.ContainsAny(char, "[]{}'\",/*:\r\n\t ") {
 		quote = constant.DoubleQuote
-		j.inStr, quoted, j.stringLabel = true, true, constant.DoubleQuote
+		j.inString, quoted, j.stringLabel = true, true, constant.DoubleQuote
 	}
-	if j.inStr && wasQuoted && inKey && (char == ":" || char == " " || char == constant.SingleQuote) {
+	if j.inString && wasQuoted && inKey && (char == ":" || char == " " || char == constant.SingleQuote) {
 		quote = constant.DoubleQuote
-		j.inStr, quoted, j.stringLabel = false, false, ""
+		j.inString, quoted, j.stringLabel = false, false, ""
 	}
 
 	return
 }
 
-func (j *Jsonc) inString(prev string, char string, next string, old string) bool {
+func (j *Jsonc) insideString(prev string, char string, next string, old string) bool {
 	charNext := char + next
-	maybeStr := (char == constant.DoubleQuote || char == constant.SingleQuote) && (!j.inStr || j.stringLabel == char)
+	maybeStr := (char == constant.DoubleQuote || char == constant.SingleQuote) && (!j.inString || j.stringLabel == char)
 	if j.comment == 0 && maybeStr && prev != constant.Esc {
-		if !j.inStr {
+		if !j.inString {
 			j.stringLabel = char
 		}
-		j.inStr = !j.inStr
-		return j.inStr
+		j.inString = !j.inString
+		return j.inString
 	}
-	if j.inStr && (charNext == `":` || charNext == `",` || charNext == `"]` || charNext == `"}`) {
-		j.inStr = old+prev != constant.Esc+constant.Esc
+	if j.inString && (charNext == `":` || charNext == `",` || charNext == `"]` || charNext == `"}`) {
+		j.inString = old+prev != constant.Esc+constant.Esc
 	}
 
-	return j.inStr
+	return j.inString
 }
 
 func (j *Jsonc) getSegments(json string, old string) (original, prev, char, next string) {
@@ -251,6 +256,6 @@ func (j *Jsonc) hasCommentEnded(char, next string) bool {
 func (j *Jsonc) reset() {
 	j.index, j.comment = 0, 0
 	j.objectDepth, j.arrayDepth = 0, 0
-	j.inStr, j.inArray, j.inObject = false, false, false
+	j.inString, j.inArray, j.inObject = false, false, false
 	j.last, j.stringLabel = "", ""
 }
