8a4d77c882e7ed2e746ae4e6dc6862276e117399
storezhang <storezhang@gmail.com>
2025-04-27 21:59:49 +0800

    perf(项目): 优化项目结构，提升启动性能

--- application.go
+++ application.go
@@ -6,7 +6,6 @@ import (
 )
 
 // Application 应用程序，可以加入两种种类型的程序
-// application.Serve 用于描述应用程序内的服务
 // application.Command 用于描述应用程序内可以被执行的命令
 // application.Argument 用于描述应用程序的参数
 type Application = core.Application
