f8ce89e4d4ac3caf4d35d517ab271909f85ca9a7
storezhang <storezhang@gmail.com>
2025-04-28 20:05:02 +0800

    docs(启动器): 增加注释

--- application.go
+++ application.go
@@ -5,9 +5,7 @@ import (
 	"github.com/harluo/boot/internal/core"
 )
 
-// Application 应用程序，可以加入两种种类型的程序
-// application.Command 用于描述应用程序内可以被执行的命令
-// application.Argument 用于描述应用程序的参数
+// Application 应用程序
 type Application = core.Application
 
 func New() *builder.Application {
--- booter.go
+++ booter.go
@@ -5,4 +5,6 @@ import (
 )
 
 // Booter 启动器
+// 实现`Commands`方法用于指定应用程序内可以被执行的命令`application.Command`
+// 实现`Arguments`方法用于指定应用程序的参数`application.Argument`
 type Booter = core.Booter
