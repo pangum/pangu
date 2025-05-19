c1ffaa47040729d8bee976621954a7d3ed5832f6
storezhang <storezhang@gmail.com>
2025-05-19 11:34:18 +0800

    build(命令): 修复info命令输出编译错误

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 require (
 	github.com/goexl/exception v0.0.3
 	github.com/goexl/log v0.0.9
-	github.com/olekukonko/tablewriter v0.0.5
+	github.com/olekukonko/tablewriter v1.0.6
 	github.com/stretchr/testify v1.10.0
 	github.com/zs5460/art v0.3.0
 )
@@ -19,11 +19,17 @@ require (
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/olekukonko/errors v1.1.0 // indirect
+	github.com/olekukonko/ll v0.0.8-0.20250516010636-22ea57d81985 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
-	go.uber.org/dig v1.18.1 // indirect
+	go.uber.org/dig v1.19.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/core/internal/command/info.go
+++ internal/core/internal/command/info.go
@@ -36,15 +36,24 @@ func (i *Info) Aliases() []string {
 
 func (i *Info) Run(_ context.Context) (err error) {
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{constant.HeaderName, constant.HeaderValue})
-	table.Append([]string{constant.ColumnName, internal.GetName()})
-	table.Append([]string{constant.ColumnVersion, internal.GetVersion()})
-	table.Append([]string{constant.ColumnBuild, internal.GetBuild()})
-	table.Append([]string{constant.ColumnComplied, internal.GetCompiled()})
-	table.Append([]string{constant.ColumnRevision, internal.GetRevision()})
-	table.Append([]string{constant.ColumnBranch, internal.GetBranch()})
-	table.Append([]string{constant.ColumnRuntime, internal.Runtime})
-	table.Render()
+	table.Header([]string{constant.HeaderName, constant.HeaderValue})
+	if ne := table.Append([]string{constant.ColumnName, internal.GetName()}); nil != ne {
+		err = ne
+	} else if ve := table.Append([]string{constant.ColumnVersion, internal.GetVersion()}); nil != ve {
+		err = ve
+	} else if bue := table.Append([]string{constant.ColumnBuild, internal.GetBuild()}); nil != bue {
+		err = bue
+	} else if ce := table.Append([]string{constant.ColumnComplied, internal.GetCompiled()}); nil != ce {
+		err = ce
+	} else if ree := table.Append([]string{constant.ColumnRevision, internal.GetRevision()}); nil != ree {
+		err = ree
+	} else if bre := table.Append([]string{constant.ColumnBranch, internal.GetBranch()}); nil != bre {
+		err = bre
+	} else if rue := table.Append([]string{constant.ColumnRuntime, internal.Runtime}); nil != rue {
+		err = rue
+	} else {
+		err = table.Render()
+	}
 
 	return
 }
